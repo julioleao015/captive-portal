ef2ca8dad9caf37a91874a9a1796e8d73eb3901d
julioleao015 <julioleao015@gmail.com>
2021-06-29 20:16:02 -0300

    Inserting column system in bootstrap grid

--- index.php
+++ index.php
@@ -14,45 +14,55 @@
 </head>
 
 <body onload="cuteAlert()">
-    <div id="div-geral" class="d-flex justify-content-center">
-        <div class="user-card bg-white">
-            <!-- Div Logo -->
-            <div class="div-logo d-flex justify-content-center">
-                <figure>
-                    <img id="img-logo" src="img/logo-absj.png" alt="Logo ABSJ" class="mb-3">
-                    <figcaption class="text-center">Associação Beneficente <br> São José</figcaption>
-                </figure>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
             </div>
-            <!-- End Logo -->
-            <hr class="line-divisor mt-1">
-            <!-- Div Form -->
-            <div id="div-form" class="d-flex justify-content-center mt-4">
-                <form action="POST" action="$PORTAL_ACTION$">
-                    <div class="input-group mb-4">
-                        <div class="input-group-append">
-                            <img class="img-input" src="img/user-solid.png" alt="Ícone do usuário">
+            <div class="col-sm">
+                <div id="div-geral" class="d-flex justify-content-center">
+                    <div class="user-card bg-white">
+                        <!-- Div Logo -->
+                        <div class="div-logo d-flex justify-content-center">
+                            <figure>
+                                <img id="img-logo" src="img/logo-absj.png" alt="Logo ABSJ" class="mb-3">
+                                <figcaption class="text-center">Associação Beneficente <br> São José</figcaption>
+                            </figure>
                         </div>
-                        <input type="text" class="form-control" name="auth_user" id="user" placeholder="Usuário">
-                        <input name="redirurl" type="hidden" value="$PORTAL_REDIRURL$">
-                        <input name="zone" type="hidden" value="$PORTAL_ZONE$">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-append">
-                            <img class="img-input" src="img/key-solid.png" alt="Ícone da senha">
+                        <!-- End Logo -->
+                        <hr class="line-divisor mt-1">
+                        <!-- Div Form -->
+                        <div id="div-form" class="d-flex justify-content-center mt-4">
+                            <form action="POST" action="$PORTAL_ACTION$">
+                                <div class="input-group mb-4">
+                                    <div class="input-group-append">
+                                        <img class="img-input" src="img/user-solid.png" alt="Ícone do usuário">
+                                    </div>
+                                    <input type="text" class="form-control" name="auth_user" id="user" placeholder="Usuário">
+                                    <input name="redirurl" type="hidden" value="$PORTAL_REDIRURL$">
+                                    <input name="zone" type="hidden" value="$PORTAL_ZONE$">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-append">
+                                        <img class="img-input" src="img/key-solid.png" alt="Ícone da senha">
+                                    </div>
+                                    <input type="password" class="form-control" name="auth_pass" id="password" placeholder="Senha">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="icon-show-passwd"><img src="img/eye-open.png" id="show-passwd" onclick="mostrarSenha()"></span>
+                                    </div>
+                                </div>
+                                <div class="form-group form-check mt-3 mb-5">
+                                    <input type="checkbox" class="form-check-input" id="check-form">
+                                    <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
+                                </div>
+                                <button type="submit" class="btn mb-3" id="btn-submit">Entrar</button>
+                            </form>
                         </div>
-                        <input type="password" class="form-control" name="auth_pass" id="password" placeholder="Senha">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="icon-show-passwd"><img src="img/eye-open.png" id="show-passwd" onclick="mostrarSenha()"></span>
-                        </div>
+                        <!-- End Form -->
                     </div>
-                    <div class="form-group form-check mt-3 mb-5">
-                        <input type="checkbox" class="form-check-input" id="check-form">
-                        <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
-                    </div>
-                    <button type="submit" class="btn mb-3" id="btn-submit">Entrar</button>
-                </form>
+                </div>
             </div>
-            <!-- End Form -->
+            <div class="col-sm">
+            </div>
         </div>
     </div>
 
